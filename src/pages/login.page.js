3f1bc426dd26b8128a2b082b6bd5ff95fe3b4ad5
NikitaVorobushek <nikvorobushek@gmail.com>
2025-12-06 22:57:24 +0300

    login page

---
+++ src/pages/login.page.js
@@ -0,0 +1,21 @@
+export class LoginPage {
+    constructor(page) {       
+        this.page = page;
+
+        this.loginBtn = page.getByRole('button', { name: 'Login' });
+
+        this.emailInput = page.getByRole('textbox', { name: 'Email' });
+        this.passwordInput =  page.getByRole('textbox', { name: 'Password' })
+    }
+
+    async login (email, password) {  
+        await this.emailInput.click();
+        await this.emailInput.fill(email);
+
+        await this.passwordInput.click();
+        await this.passwordInput.fill(password);  
+
+        await this.loginBtn.click();  
+    }
+
+    }
